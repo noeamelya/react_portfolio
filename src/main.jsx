3da4ad9f89cfab41d8e238602da44343e6fc27b4
noeamelya <noeamelyaofficial@hotmail.com>
2023-09-18 15:17:55 +0100

    update function render component project

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+'bootstrap/dist/css/bootstrap.min.css'; 
+import * as bootstrap from 'bootstrap';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
